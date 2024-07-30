b3bcf166803096063c46f5d055e15e74195c0d4f
Enes Tekin <enestekin3457@gmail.com>
2024-07-30 19:40:35 +0300

    Angular

---
+++ data.js
@@ -0,0 +1,77 @@
+module.exports = function () {
+    return {
+        products: [
+            { id: 1, name: "Samsung Galaxy S22", price: 18500, imageUrl: '1.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 2, name: "Samsung Galaxy S21", price: 16800, imageUrl: '2.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 3, name: "Samsung Galaxy S20", price: 9600, imageUrl: '3.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 4, name: "Samsung Galaxy S10", price: 6800, imageUrl: '4.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+            { id: 5, name: "Samsung Galaxy S9", price: 4900, imageUrl: '5.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+            { id: 6, name: "Samsung Galaxy S8", price: 4400, imageUrl: '6.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+            { id: 7, name: "Samsung Galaxy S7", price: 4200, imageUrl: '7.jpg', description: 'İkinci El', category: 'Telefon' },
+            { id: 8, name: "Samsung Galaxy S6", price: 3200, imageUrl: '8.jpg', description: 'İkinci El', category: 'Telefon' },
+
+            { id: 9, name: "Samsung Galaxy A90", price: 9900, imageUrl: '9.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+            { id: 10, name: "Samsung Galaxy A82", price: 9200, imageUrl: '10.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+            { id: 11, name: "Samsung Galaxy A80", price: 8600, imageUrl: '11.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+            { id: 12, name: "Samsung Galaxy A73", price: 22000, imageUrl: '12.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 13, name: "Samsung Galaxy A72", price: 19000, imageUrl: '13.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 14, name: "Samsung Galaxy A71", price: 18500, imageUrl: '14.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 15, name: "Samsung Galaxy A70", price: 5000, imageUrl: '15.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+            { id: 16, name: "Samsung Galaxy A54", price: 18000, imageUrl: '16.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 17, name: "Samsung Galaxy A53", price: 16800, imageUrl: '17.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 18, name: "Samsung Galaxy A52", price: 15900, imageUrl: '18.jpg', description: 'Sıfır Ürün', category: 'Telefon' },
+            { id: 19, name: "Samsung Galaxy A51", price: 6000, imageUrl: '19.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+            { id: 20, name: "Samsung Galaxy A50", price: 4200, imageUrl: '20.jpg', description: 'Yenilenmiş Ürün', category: 'Telefon' },
+
+            // { id: 21, name: "Samsung Galaxy A43", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 22, name: "Samsung Galaxy A42", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 23, name: "Samsung Galaxy A41", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 24, name: "Samsung Galaxy A40", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 25, name: "Samsung Galaxy A35", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 26, name: "Samsung Galaxy A34", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 27, name: "Samsung Galaxy A33", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 28, name: "Samsung Galaxy A32", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 39, name: "Samsung Galaxy A31", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 30, name: "Samsung Galaxy A30", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 31, name: "Samsung Galaxy A24", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 32, name: "Samsung Galaxy A23", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 33, name: "Samsung Galaxy A22", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 34, name: "Samsung Galaxy A21", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 35, name: "Samsung Galaxy A20", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 36, name: "Samsung Galaxy A13", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 37, name: "Samsung Galaxy A12", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 38, name: "Samsung Galaxy A11", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+            // { id: 39, name: "Samsung Galaxy A10", price: 10000, imageUrl: '.jpg', description: '', category: 'Telefon' },
+
+            { id: 40, name: "Samsung Galaxy Tab S8", price: 25400, imageUrl: '40.jpg', description: 'Sıfır Ürün', category: 'Tablet' },
+            { id: 41, name: "Samsung Galaxy Tab A8", price: 11000, imageUrl: '41.jpg', description: 'Sıfır Ürün', category: 'Tablet' },
+            { id: 42, name: "Samsung Galaxy Tab S7", price: 9900, imageUrl: '42.jpg', description: 'Sıfır Ürün', category: 'Tablet' },
+            { id: 43, name: "Samsung Galaxy Tab A7", price: 13000, imageUrl: '43.jpg', description: 'Sıfır Ürün', category: 'Tablet' },
+            { id: 44, name: "Samsung Galaxy Tab S6", price: 9800, imageUrl: '44.jpg', description: 'Sıfır Ürün', category: 'Tablet' },
+            { id: 45, name: "Samsung Galaxy Tab S5", price: 11000, imageUrl: '45.jpg', description: 'İkinci El', category: 'Tablet' },
+            { id: 46, name: "Samsung Galaxy Tab S4", price: 6000, imageUrl: '46.jpg', description: 'İkinci El', category: 'Tablet' },
+            { id: 47, name: "Samsung Galaxy Tab S3", price: 4000, imageUrl: '47.jpg', description: 'İkinci El', category: 'Tablet' },
+            { id: 48, name: "Samsung Galaxy TabPro", price: 3600, imageUrl: '48.jpg', description: 'İkinci El', category: 'Tablet' },
+            { id: 49, name: "Samsung Galaxy Tab S2", price: 2900, imageUrl: '49.jpg', description: 'İkinci El', category: 'Tablet' },
+
+            { id: 50, name: "SAMSUNG QN900D 75inç", price: 276000, imageUrl: '50.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 51, name: "SAMSUNG QN90D 55 inç", price: 81700, imageUrl: '51.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 52, name: "SAMSUNG QE75Q60DAUXTK 75 inç", price: 67900, imageUrl: '52.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 53, name: "SAMSUNG QE55QN85DBTXTK 55 inç", price: 57400, imageUrl: '53.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 54, name: "SAMSUNG UE75DU8000UXTK 75 inç", price: 55200, imageUrl: '54.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 55, name: "SAMSUNG DU7000 65 inç", price: 39900, imageUrl: '55.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 56, name: "SAMSUNG UE50CU8500 50 inç", price: 29000, imageUrl: '56.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 57, name: "SAMSUNG 50CU7000 50 inç", price: 24800, imageUrl: '57.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 58, name: "SAMSUNG UE43CU8000UXTK 43 inç", price: 21200, imageUrl: '58.jpg', description: 'Sıfır Ürün', category: 'Televizyon' },
+            { id: 59, name: "SAMSUNG UE32T5300ADXTK 32 inç", price: 15600, imageUrl: '59.jpg', description: 'İkinci El', category: 'Televizyon' },
+
+
+        ],
+        categories: [
+            { id: 1, name: "Telefon" },
+            { id: 2, name: "Tablet" },
+            { id: 3, name: "Televizyon" }
+        ],
+        orders: []
+    }
+}
